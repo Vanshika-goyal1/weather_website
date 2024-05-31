d0b69c90f2fa25a3532de586b7fffe793af7a823
Vanshika Goyal <vanshikagoyal1704@gmail.com>
2024-05-31 23:07:10 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b7a2b5f816mshe5eff7e3def6443p157176jsn6cc825327587',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
